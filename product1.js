ed874b64d97e1bafed46875d9b4ff3c9db7c6984
chenyuchen00 <414401127@m365.fju.edu.tw>
2026-05-01 00:23:43 +0800

    color

--- product1.js
+++ product1.js
@@ -20,3 +20,16 @@ function scrollToTop() {
         behavior: 'smooth' // 關鍵：平滑滾動效果
     });
 }
+function changeHero(imgUrl, colorName) {
+    // 抓取大圖並更換背景
+    const hero = document.getElementById('main-hero');
+    if (hero) {
+        hero.style.backgroundImage = "url('" + imgUrl + "')";
+    }
+
+    // 抓取文字標籤並更換文字
+    const label = document.getElementById('color-label');
+    if (label) {
+        label.innerText = colorName;
+    }
+}
